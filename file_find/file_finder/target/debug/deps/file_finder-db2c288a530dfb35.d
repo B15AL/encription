a9422411df0713458d6722482cd01108391a97bb
Bishal Roy <broy353671@gmail.com>
2026-08-05 23:41:12 +0530

    normal file finder in dir

---
+++ file_find/file_finder/target/debug/deps/file_finder-db2c288a530dfb35.d
@@ -0,0 +1,5 @@
+/home/bishal-roy/work/maldev/ransome_ware/file_encript/file_find/file_finder/target/debug/deps/file_finder-db2c288a530dfb35.d: src/main.rs
+
+/home/bishal-roy/work/maldev/ransome_ware/file_encript/file_find/file_finder/target/debug/deps/file_finder-db2c288a530dfb35: src/main.rs
+
+src/main.rs:
---
+++ file_find/file_finder/target/debug/file_finder.d
@@ -0,0 +1,1 @@
+/home/bishal-roy/work/maldev/ransome_ware/file_encript/file_find/file_finder/target/debug/file_finder: /home/bishal-roy/work/maldev/ransome_ware/file_encript/file_find/file_finder/src/main.rs
